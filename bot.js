0bc23f24adb9941c9768f0671079187290bff77c
Candy <46304801+meowcandy@users.noreply.github.com>
2019-02-15 17:38:53 +0700

    Add files via upload

--- bot.js
+++ bot.js
@@ -367,7 +367,7 @@ Client.on('message', (message)=>{
         purge();
     }
     if(message.content.startsWith(prefix + "kkswi")) {
-        if (!message.member.roles.find("name", "Sưi 's role", "Sưi only")) {
+        if (!message.member.roles.find("name", "Sưi 's role", "Sưi only", "pro.")) {
             message.channel.send('Oof, giáo sư ko dùng đc lệnh này rồi :3')
             return
         }
